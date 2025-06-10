2b9ea3372dee4dc225d3f4ad6197b5e546ebdd7a
Shmuli2979 <Shmuligsph@gmail.com>
2025-06-10 00:04:27 -0400

    added

--- Demonstration.js
+++
@@ -1,8 +0,0 @@
-const userJS = {name: "Shmuli", age: 23};
-const json = JSON.stringify(userJS);
-console.log(`${json} Demonstration from JS to JSON` );
-
-
-const userJSON = '{"name": "Shmuli", "age": 23}';
-const JS = JSON.parse(userJSON);
-console.log(`${JS.name} ${JS.age} Demonstration from JSON to JS`);
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-require("gimatria")
+import greet from "./greet.js"
+
+import "gimatria"
 console.log((5785).gimatria());
 console.log("ה'תשפה".gimatria())
+
+console.log(`That was the NPM module, now to the Demonstration and Import-Export:`)
+
+const userJS = {name: "Shmuli", age: 23};
+const json = JSON.stringify(userJS);
+console.log(`${json} Demonstration from JS to JSON` );
+
+
+const userJSON = '{"name": "Shmuli", "age": 23}';
+const JS = JSON.parse(userJSON);
+console.log(`${JS.name} ${JS.age} Demonstration from JSON to JS`);
+
+console.log(greet("Shmuli"))
